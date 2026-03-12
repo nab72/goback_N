de4dc7fcc7b38432c2f0acac88ba73689b00769e
nab72 <151378045+nab72@users.noreply.github.com>
2026-03-12 11:54:18 +0530

    Implement multithreading feature in sam.c
    Added a sample multithreading feature using pthreads.

--- sam.c
+++ sam.c
@@ -1,4 +1,18 @@
-MediaKeyMessageEvent\
-hello guys welcome back to my youtube channel
-to test-featureeeeee yeeee
-again switched to test feature
+#implemented sample multithreading feature
+#include <pthread.h>
+#include <stdio.h>
+
+void* foo(void* arg) {
+    printf("Created a new thread");
+    return NULL;
+ }
+int main() {
+    
+    // Create a pthread_t variable to store
+    // thread ID
+    pthread_t thread1;
+    
+    // Creating a new thread. 
+    pthread_create(&thread1, NULL, foo, NULL);
+    return 0;
+}
